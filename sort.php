fd0741c4ee00914c91859544e274cd93c06795c8
artanthos <alexandru.okros@live.com>
2017-05-21 15:07:43 +0300

    Add files via upload

--- sort.php
+++ sort.php
@@ -1,111 +1,121 @@
 <?php
-	class mergeSortNulled {
-		private $argsFromCLI;
-		private $CLIErrorForThis;
-		private $fileToReadFrom;
-		private $db;
-		
-		public function __construct() {
-			$this->setIOFromCLIEnvironment();
-		}
-		
-			//CLI related
-			private function setIOFromCLIEnvironment() {
-				$shortOpts  = 'i:o:';
-				$IOtypes = array('input', 'output', 'db');
-				
-				foreach($IOtypes as $IOtype){
-					$longOpts[] = $IOtype.':';
-				}
-				
-				$this->argsFromCLI = getopt($shortOpts, $longOpts);
-				
-				foreach($IOtypes as $IOtype){
-					$this->seeIfCLIProvidedThis($IOtype);
-					if(!isset($this->CLIErrorForThis[$IOtype])){
-						$this->letsProceedAndSetThis($IOtype);
-					} else {
-						print $this->CLIErrorForThis[$IOtype];
-					}
-				}
-			}
-				
-				private function seeIfCLIProvidedThis($IOtype){
-					try {
-						$this->checkForThis($IOtype);
-					} catch (Exception $e) {
-						$this->CLIErrorForThis[$IOtype] = $e->getMessage();
-					}
-				}
-				
-					private function checkForThis($IOtype) {
-						if(!isset($this->argsFromCLI[$IOtype])) {
-							throw new InvalidArgumentException("\n --{$IOtype} arg required\n");
-						}
-					}
-		
-			private function letsProceedAndSetThis($IOtype){
-				switch($IOtype){
-					case 'input' :
-						$this->fileToReadFrom = $this->argsFromCLI[$IOtype];
-						break;
-					case 'output' :
-						$this->fileToWriteTo = $this->argsFromCLI[$IOtype];
-						break;
-					case 'db' :
-						$this->db = new SQLite3($IOtype);
-						break;
-				}
-			}
-		
-		// DB related
-		public function importData(){
-			$this->checkIfCLISetDbElseEXIT();
+    class mergeSortNulled
+    {
+        private $argsFromCLI;
+        private $CLIErrorForThis;
+        private $fileToReadFrom;
+        private $db;
+        
+        public function __construct()
+        {
+            $this->setIOFromCLIEnvironment();
+        }
+        
+            //CLI related
+            private function setIOFromCLIEnvironment()
+            {
+                $shortOpts  = 'i:o:';
+                $IOtypes = array('input', 'output', 'db');
+                
+                foreach ($IOtypes as $IOtype) {
+                    $longOpts[] = $IOtype.':';
+                }
+                
+                $this->argsFromCLI = getopt($shortOpts, $longOpts);
+                
+                foreach ($IOtypes as $IOtype) {
+                    $this->seeIfCLIProvidedThis($IOtype);
+                    if (!isset($this->CLIErrorForThis[$IOtype])) {
+                        $this->letsProceedAndSetThis($IOtype);
+                    } else {
+                        print $this->CLIErrorForThis[$IOtype];
+                    }
+                }
+            }
+                
+        private function seeIfCLIProvidedThis($IOtype)
+        {
+            try {
+                $this->checkForThis($IOtype);
+            } catch (Exception $e) {
+                $this->CLIErrorForThis[$IOtype] = $e->getMessage();
+            }
+        }
+                
+        private function checkForThis($IOtype)
+        {
+            if (!isset($this->argsFromCLI[$IOtype])) {
+                throw new InvalidArgumentException("\n --{$IOtype} arg required\n");
+            }
+        }
+        
+        private function letsProceedAndSetThis($IOtype)
+        {
+            switch ($IOtype) {
+                    case 'input':
+                        $this->fileToReadFrom = $this->argsFromCLI[$IOtype];
+                        break;
+                    case 'output':
+                        $this->fileToWriteTo = $this->argsFromCLI[$IOtype];
+                        break;
+                    case 'db':
+                        $this->db = new SQLite3($IOtype);
+                        break;
+                }
+        }
+        
+        // DB related
+        public function importData()
+        {
+            $this->checkIfCLISetDbElseEXIT();
 
-			$inputFileHandler = fopen($this->fileToReadFrom, "r");
-			$lastExplodedNumberFromPreviousChunkFile = null;
-			if ($inputFileHandler){
-				while (($thisChunkFile = fgets($inputFileHandler, 2048)) !== false){
-					$numbersArrayInChunk = explode(',', $thisChunkFile);
-					if($lastExplodedNumberFromPreviousChunkFile !== null) {
-						$numbersArrayInChunk[0] = (int) $lastExplodedNumberFromPreviousChunkFile . $numbersArrayInChunk[0];
-					}
-					$lastExplodedNumberFromPreviousChunkFile = array_pop($numbersArrayInChunk);
-					$this->writeToDb($numbersArrayInChunk);
-				}
-				
-				if (!feof($inputFileHandler)) {
-					print "Error: unexpected fgets() fail\n";
-				}
-				
-				fclose($inputFileHandler);
-			}
-		}
-			private function checkIfCLISetDbElseEXIT(){
-				if($this->db===NULL){
-					exit;
-				}
-				
-				$this->db->exec('DROP TABLE IF EXISTS numbers');
-				$this->db->exec('CREATE TABLE numbers (value INTEGER)');
-			}
-		
-			private function writeToDb($numbers) {
-				foreach($numbers as $number) {
-					$this->db->exec('INSERT INTO numbers VALUES (' . $number . ')');
-				}
-			}
+            $inputFileHandler = fopen($this->fileToReadFrom, "r");
+            $lastExplodedNumberFromPreviousChunkFile = null;
+            if ($inputFileHandler) {
+                while (($thisChunkFile = fgets($inputFileHandler, 2048)) !== false) {
+                    $numbersArrayInChunk = explode(',', $thisChunkFile);
+                    if ($lastExplodedNumberFromPreviousChunkFile !== null) {
+                        $numbersArrayInChunk[0] = (int) $lastExplodedNumberFromPreviousChunkFile . $numbersArrayInChunk[0];
+                    }
+                    $lastExplodedNumberFromPreviousChunkFile = array_pop($numbersArrayInChunk);
+                    $this->writeToDb($numbersArrayInChunk);
+                }
+                
+                if (!feof($inputFileHandler)) {
+                    print "Error: unexpected fgets() fail\n";
+                }
+                
+                fclose($inputFileHandler);
+            }
+        }
+        private function checkIfCLISetDbElseEXIT()
+        {
+            if ($this->db===null) {
+                exit;
+            }
+                
+            $this->db->exec('DROP TABLE IF EXISTS numbers');
+            $this->db->exec('CREATE TABLE numbers (value INTEGER)');
+        }
+        
+        private function writeToDb($numbers)
+        {
+            foreach ($numbers as $number) {
+                $this->db->exec('INSERT INTO numbers VALUES (' . $number . ')');
+            }
+        }
 
-		public function exportData(){
-			$outputFileHandler = @fopen($this->fileToWriteTo, "w");
-			$results = $this->db->query('SELECT * FROM numbers ORDER BY value');
-			while ($row = $results->fetchArray()) {
-				fwrite($outputFileHandler, $row['value'] . ',');
-			}
-			fclose($outputFileHandler);
-		}
-	}
+        public function exportData()
+        {
+            $outputFileHandler = @fopen($this->fileToWriteTo, "w");
+            $results = $this->db->query('SELECT * FROM numbers ORDER BY value');
+            while ($row = $results->fetchArray()) {
+                fwrite($outputFileHandler, $row['value'] . ',');
+            }
+            fclose($outputFileHandler);
+        }
+    }
 
-	$sort = new mergeSortNulled();
-	$sort->importData();
-	$sort->exportData();
+    $sort = new mergeSortNulled();
+    $sort->importData();
+    $sort->exportData();
